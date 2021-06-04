a833bf921141b13f93e1451fdfaf9f862de560ca
Dalton Willard <willarddalton@gmail.com>
2021-06-04 13:54:27 -0600

    Leaderboard update and retrieval methods are in place

--- speed/game/leaderboard.py
+++ speed/game/leaderboard.py
@@ -10,8 +10,21 @@ class Leaderboard(Console):
         super().__init__()
         self.__board = self.__load_leaderboard()
 
-    def new_entry(self, player=str, score=int:
-        pass
+    def new_entry(self, player=str, score=int):
+        timestamp = datetime.now().strftime("%b %-d %Y  %Y%m%d%H%M%S")
+
+        self.__leaderboard.update(
+            {timestamp: {
+                "score": score,
+                "player": player}})
+
+        new_dict = {k: v for (k, v) in sorted(
+                self.__leaderboard.items(),
+                key=lambda x: x[1]["score"])}
+        with open("mastermind/assets/leaderboard.json", "w") as data:
+            dump(new_dict, data, indent=4)
+
+        self.__board = self.__load_leaderboard()
 
     def __load_leaderboard(self):
         with open("speed/assets/leaderboard.json", "r") as data:
