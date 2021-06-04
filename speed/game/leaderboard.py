73d894d85aeb7e7a237ac3182dc8aa86e624577b
Dalton Willard <willarddalton@gmail.com>
2021-06-03 21:14:33 -0600

    Basic structure, environment and code stubs in play. Code starts without bugs

---
+++ speed/game/leaderboard.py
@@ -0,0 +1,19 @@
+class Leaderboard:
+    """A code template for a person who directs the game. The responsibility of
+    this class of objects is to control the sequence of play.
+
+    Stereotype:
+        Service Provider, Interfacer
+
+    Attributes:
+        roster (Roster): An instance of the class of objects known as Roster.
+    """
+
+    def __init__(self):
+        """The class constructor.
+
+        Args:
+            self (Console): an instance of Console.
+        """
+        pass
+    
