9d4fba94be19f451b9394f6de990c5dee015c08b
Dalton Willard <willarddalton@gmail.com>
2021-06-05 19:39:01 -0600

    Removed superfluous Buffer class, refactored Director class to compensate

--- speed/game/director.py
+++ speed/game/director.py
@@ -1,8 +1,9 @@
 from time import sleep
+
 from game import constants
 from game.words import Words
 from game.scoreboard import Scoreboard
-from game.buffer import Buffer
+
 
 # AGNES check import names with GitHub.
 
@@ -31,25 +32,21 @@ class Director:
             self (Director): an instance of Director.
         """
         # (AH) Class attributes.
+        self._scoreboard = Scoreboard()
         self._words = Words()
         self._input_service = input_service
-        self._keep_playing = True
         self._output_service = output_service
-        self._scoreboard = Scoreboard()
-        self._buffer = Buffer()
-        # (AH) see get_inputs().
-        self.letter = None
 
-    def start_game(self, player):
+        self.buffer = []
+        self._keep_playing = True
+
+    def start_game(self, player: str):
         """Starts the game loop to control the sequence of play.
 
-        Args:
-            self (Director): an instance of Director.
+        ARGS
+        player : str
+            The username of a selected player profile.
         """
-        # (AH) changed from 'input' to print.
-        print(f"Game started. Player is {player}")
-
-        # (AH) Loop is for each keystroke at a time.
         while self._keep_playing:
             self._get_inputs()
             # (AH) update to compare only when typed <enter>.
@@ -59,35 +56,27 @@ class Director:
             sleep(constants.FRAME_LENGTH)
 
     def _get_inputs(self):
-        """Gets the inputs at the beginning of each round of play.
-        In this case,
-        that means getting the words typed by the player.
-
-        Args:
-            self (Director): An instance of Director.
         """
-
-        # Agnes check with Sarah's code.
-        self.letter = self.input_service.get_letter()
-        if self.letter.isalpha():
-            self._buffer.update_buffer(self.letter)
+        Gets the keyboard input at the beginning of each play cycle.
+        If input is a letter, it is added to the buffer.
+        """
+        letter = self.input_service.get_letter()
+        if letter.isalpha():
+            self.buffer.append(letter)
 
     def _do_updates(self):
         """Updates the important game information for each round of play.
-        In this case, that means checking that word typed by player
-        in Buffer is the same as the random word from the word list.
-
-        Args:
-            self (Director): An instance of Director.
+        In this case, that means checking that word typed by player in Buffer
+        is the same as the random word from the word list.
         """
         # (AH) player_input is what player typed and is stored in Buffer Class.
-        player_input = self._buffer.get_buffer()
+        player_input = "".join(self.buffer)
         # (AH) player_input is compared to the key of the Words dictionary.
         if player_input in self.words.get_words():
             # (AH) if player_input correct, increment score.
             self.scoreboard.add_score(1)
             # (AH) clear the Buffer list for next round of play.
-            self.buffer.flush_buffer()
+            self.buffer = []
 
     def _do_outputs(self):
         """Outputs the important game information for each round of play.
@@ -101,4 +90,4 @@ class Director:
         self._output_service.draw_top()
         self._output_service.draw_word()
         self._output_service.draw_bottom()
-        self._output_service.flush_buffer()
+        self.buffer = []
